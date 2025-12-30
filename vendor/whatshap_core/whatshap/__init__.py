ac827a9c2d376e91f86688601e8f871690e81b1b
CagySloth <ngcl1013@gmail.com>
2025-12-31 02:02:43 +0800

    pytest bugfix

--- vendor/whatshap_core/whatshap/__init__.py
+++ vendor/whatshap_core/whatshap/__init__.py
@@ -1,5 +1,22 @@
-# vendor/whcore/py/__init__.py
+# vendor/whatshap_core/whatshap/__init__.py
 
-from . import core          # compiled from core.pyx + src/*
-from . import readselect    # compiled from readselect.pyx
-from . import priorityqueue # compiled from priorityqueue.pyx
+import sys
+
+# On Linux, other compiled extensions (readselect/priorityqueue) may need C++ symbols
+# from core.so. Make core load with RTLD_GLOBAL so its symbols are visible.
+try:
+    import ctypes
+    RTLD_GLOBAL = ctypes.RTLD_GLOBAL
+except Exception:
+    RTLD_GLOBAL = 0
+
+if hasattr(sys, "getdlopenflags") and RTLD_GLOBAL:
+    _old_flags = sys.getdlopenflags()
+    sys.setdlopenflags(_old_flags | RTLD_GLOBAL)
+    from . import core
+    sys.setdlopenflags(_old_flags)
+else:
+    from . import core
+
+from . import readselect
+from . import priorityqueue
